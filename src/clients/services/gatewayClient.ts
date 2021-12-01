6320557ec611af5bf92ff1ffe5f743cf5b20d93f
Anguiar <shacharroy14@gmail.com>
2021-12-01 10:53:04 +0200

    feat: support basic authentication for gateway

--- src/clients/services/gatewayClient.ts
+++ src/clients/services/gatewayClient.ts
@@ -2,18 +2,27 @@ import { Readable } from 'stream';
 import { inject, singleton } from 'tsyringe';
 import { Logger } from '@map-colonies/js-logger';
 import { HttpClient, IHttpRetryConfig } from '@map-colonies/mc-utils';
-import { IConfig } from '../../common/interfaces';
+import { AxiosBasicCredentials } from 'axios';
+import { IGatewayAuthConfig, IConfig } from '../../common/interfaces';
 import { Services } from '../../common/constants';
 
 @singleton()
 export class GatewayClient extends HttpClient {
-  public gatewayImageRouteId: string;
-  public gatewayJsonRouteId: string;
+  private readonly gatewayImageRouteId: string;
+  private readonly gatewayJsonRouteId: string;
+  private readonly gatewayAuthConfig: IGatewayAuthConfig;
+  private readonly authOptions?: AxiosBasicCredentials;
 
   public constructor(@inject(Services.LOGGER) logger: Logger, @inject(Services.CONFIG) private readonly config: IConfig) {
     super(logger, config.get<string>('gateway.url'), 'GatewayClient', config.get<IHttpRetryConfig>('httpRetry'));
     this.gatewayImageRouteId = this.config.get<string>('gateway.imageRouteId');
     this.gatewayJsonRouteId = this.config.get<string>('gateway.jsonRouteId');
+    this.gatewayAuthConfig = this.config.get<IGatewayAuthConfig>('gateway.auth');
+
+    if (this.gatewayAuthConfig.enabled) {
+      const { enabled, ...authOptions } = this.gatewayAuthConfig;
+      this.authOptions = authOptions;
+    }
   }
 
   public async uploadImageToGW(data: Buffer | Readable, filename: string): Promise<void> {
@@ -25,13 +34,13 @@ export class GatewayClient extends HttpClient {
   }
 
   private async internalUploadFile(routeId: string, data: Buffer | Readable, filename: string): Promise<void> {
-    this.axiosOptions.headers = {
-      'content-type': 'application/octet-stream',
+    const addedHeaders = {
+      'Content-Type': 'application/octet-stream',
     };
     const queryParams = {
       filename: encodeURIComponent(filename),
       routeId: routeId,
     };
-    await this.post('/', data, queryParams);
+    await this.post('/', data, queryParams, undefined, this.authOptions, addedHeaders);
   }
 }
