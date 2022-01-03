07d7d3adb156d36f010a6eb18745a32b773411a3
Shahar s <cl-shaharshavit-rnd@users.noreply.github.com>
2022-01-03 10:13:19 +0200

    chore: covrage

--- tests/configurations/unit/jest.config.js
+++ tests/configurations/unit/jest.config.js
@@ -28,7 +28,7 @@ module.exports = {
   testEnvironment: 'node',
   coverageThreshold: {
     global: {
-      branches: 62,
+      branches: 55,
       functions: 80,
       lines: 80,
       statements: -10,
